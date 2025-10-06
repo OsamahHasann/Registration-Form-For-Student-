2635ac063e937bd2ecd1759d9647d912d931233c
Eng \ Osamah dawood <osamahhasan112@gmail.com>
2025-10-07 00:42:29 +0300

    first commit

---
+++ Rigistrion_form/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Rigistrion_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.show_data = new System.Windows.Forms.TextBox();
+            this.ro_female = new System.Windows.Forms.RadioButton();
+            this.ro_mule = new System.Windows.Forms.RadioButton();
+            this.lb_stu_level = new System.Windows.Forms.TextBox();
+            this.lb_id = new System.Windows.Forms.TextBox();
+            this.lb_email = new System.Windows.Forms.TextBox();
+            this.lb_password = new System.Windows.Forms.TextBox();
+            this.lb_username = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(34, 432);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(556, 29);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(28, 432);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(34, 434);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(522, 27);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.MistyRose;
+            this.panel5.Controls.Add(this.btn_submit);
+            this.panel5.Controls.Add(this.show_data);
+            this.panel5.Controls.Add(this.ro_female);
+            this.panel5.Controls.Add(this.ro_mule);
+            this.panel5.Controls.Add(this.lb_stu_level);
+            this.panel5.Controls.Add(this.lb_id);
+            this.panel5.Controls.Add(this.lb_email);
+            this.panel5.Controls.Add(this.lb_password);
+            this.panel5.Controls.Add(this.lb_username);
+            this.panel5.Controls.Add(this.label6);
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Controls.Add(this.label1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(34, 29);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(522, 405);
+            this.panel5.TabIndex = 4;
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.Color.Peru;
+            this.btn_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_submit.Location = new System.Drawing.Point(30, 199);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 14;
+            this.btn_submit.Text = "Submit";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // show_data
+            // 
+            this.show_data.Location = new System.Drawing.Point(30, 228);
+            this.show_data.Multiline = true;
+            this.show_data.Name = "show_data";
+            this.show_data.Size = new System.Drawing.Size(452, 150);
+            this.show_data.TabIndex = 13;
+            // 
+            // ro_female
+            // 
+            this.ro_female.AutoSize = true;
+            this.ro_female.Location = new System.Drawing.Point(163, 172);
+            this.ro_female.Name = "ro_female";
+            this.ro_female.Size = new System.Drawing.Size(59, 17);
+            this.ro_female.TabIndex = 12;
+            this.ro_female.TabStop = true;
+            this.ro_female.Text = "Female";
+            this.ro_female.UseVisualStyleBackColor = true;
+            // 
+            // ro_mule
+            // 
+            this.ro_mule.AutoSize = true;
+            this.ro_mule.Location = new System.Drawing.Point(69, 172);
+            this.ro_mule.Name = "ro_mule";
+            this.ro_mule.Size = new System.Drawing.Size(47, 17);
+            this.ro_mule.TabIndex = 11;
+            this.ro_mule.TabStop = true;
+            this.ro_mule.Text = "Mule";
+            this.ro_mule.UseVisualStyleBackColor = true;
+            // 
+            // lb_stu_level
+            // 
+            this.lb_stu_level.Location = new System.Drawing.Point(74, 137);
+            this.lb_stu_level.Name = "lb_stu_level";
+            this.lb_stu_level.Size = new System.Drawing.Size(121, 20);
+            this.lb_stu_level.TabIndex = 10;
+            // 
+            // lb_id
+            // 
+            this.lb_id.Location = new System.Drawing.Point(74, 107);
+            this.lb_id.Name = "lb_id";
+            this.lb_id.Size = new System.Drawing.Size(121, 20);
+            this.lb_id.TabIndex = 9;
+            // 
+            // lb_email
+            // 
+            this.lb_email.Location = new System.Drawing.Point(74, 74);
+            this.lb_email.Name = "lb_email";
+            this.lb_email.Size = new System.Drawing.Size(200, 20);
+            this.lb_email.TabIndex = 8;
+            // 
+            // lb_password
+            // 
+            this.lb_password.Location = new System.Drawing.Point(74, 46);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(121, 20);
+            this.lb_password.TabIndex = 7;
+            // 
+            // lb_username
+            // 
+            this.lb_username.AccessibleName = "user_name";
+            this.lb_username.Location = new System.Drawing.Point(74, 19);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(200, 20);
+            this.lb_username.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Study Level";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User name ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Registrion_form";
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.TextBox show_data;
+        private System.Windows.Forms.RadioButton ro_female;
+        private System.Windows.Forms.RadioButton ro_mule;
+        private System.Windows.Forms.TextBox lb_stu_level;
+        private System.Windows.Forms.TextBox lb_id;
+        private System.Windows.Forms.TextBox lb_email;
+        private System.Windows.Forms.TextBox lb_password;
+        private System.Windows.Forms.TextBox lb_username;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rigistrion_form/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rigistrion_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string user_name;
+        string password;
+        string email;
+        string id;
+        string study_level;
+        string gender;
+
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            user_name = lb_username.Text;
+            password = lb_password.Text;
+            email = lb_email.Text;
+            id = lb_id.Text;
+            study_level = lb_email.Text;
+            gender = ro_mule.Text;
+
+            if (!Regex.IsMatch(user_name, "\\w+\\s?\\w+(\\d)?"))
+            {
+                MessageBox.Show("User name is unvalidion");
+                return;
+            }
+            if (!Regex.IsMatch(password, "^\\d{5,}$"))
+            {
+                MessageBox.Show("password is unvalidion");
+                return;
+            }
+
+            if (!Regex.IsMatch(email, "\\w+\\d*?@\\w+.\\w"))
+            {
+                MessageBox.Show("email is unvalidion");
+                return;
+            }
+
+            if (!Regex.IsMatch(id, "^\\d{8}"))
+            {
+                MessageBox.Show("id name is unvalidion");
+                return;
+            }
+
+            if (!Regex.IsMatch(study_level, "\\w+"))
+            {
+                MessageBox.Show("study_level is unvalidion");
+                return;
+            }
+            if (gender == null)
+            {
+                gender = "Female";
+            }
+            else
+            {
+                gender = "Mule";
+            }
+            
+          
+            show_data.Text = "User Name: "+user_name+"\n"
+                             +"Email: " +email+"\n"
+                             +"ID: "+id+"\n"
+                             +"Gender: "+gender;
+
+        }
+    }
+}
