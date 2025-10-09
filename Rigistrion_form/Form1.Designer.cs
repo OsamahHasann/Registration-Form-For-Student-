02d260b02ab3801062b3085a65438f2f457ba3c7
Osamahhasann <osamahhasan112@gmail.com>
2025-10-10 01:54:20 +0300

    add some fileds to form

--- Rigistrion_form/Form1.Designer.cs
+++ Rigistrion_form/Form1.Designer.cs
@@ -48,7 +48,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.lbl_selected_color = new System.Windows.Forms.Label();
+            this.btn_pick_color = new System.Windows.Forms.Button();
+            this.dtp_birthdate = new System.Windows.Forms.DateTimePicker();
+            this.lb_birth_day = new System.Windows.Forms.Label();
+            this.cmb_country = new System.Windows.Forms.ComboBox();
+            this.lb_country = new System.Windows.Forms.Label();
+            this.pic_student = new System.Windows.Forms.PictureBox();
+            this.btn__reset = new System.Windows.Forms.Button();
+            this.btn_upload = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_load = new System.Windows.Forms.Button();
             this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_student)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -86,6 +98,17 @@
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.MistyRose;
+            this.panel5.Controls.Add(this.btn_load);
+            this.panel5.Controls.Add(this.btn_save);
+            this.panel5.Controls.Add(this.btn_upload);
+            this.panel5.Controls.Add(this.btn__reset);
+            this.panel5.Controls.Add(this.pic_student);
+            this.panel5.Controls.Add(this.lb_country);
+            this.panel5.Controls.Add(this.cmb_country);
+            this.panel5.Controls.Add(this.lb_birth_day);
+            this.panel5.Controls.Add(this.dtp_birthdate);
+            this.panel5.Controls.Add(this.btn_pick_color);
+            this.panel5.Controls.Add(this.lbl_selected_color);
             this.panel5.Controls.Add(this.btn_submit);
             this.panel5.Controls.Add(this.show_data);
             this.panel5.Controls.Add(this.ro_female);
@@ -111,7 +134,7 @@
             // 
             this.btn_submit.BackColor = System.Drawing.Color.Peru;
             this.btn_submit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_submit.Location = new System.Drawing.Point(30, 199);
+            this.btn_submit.Location = new System.Drawing.Point(13, 289);
             this.btn_submit.Name = "btn_submit";
             this.btn_submit.Size = new System.Drawing.Size(75, 23);
             this.btn_submit.TabIndex = 14;
@@ -121,16 +144,17 @@
             // 
             // show_data
             // 
-            this.show_data.Location = new System.Drawing.Point(30, 228);
+            this.show_data.Location = new System.Drawing.Point(10, 349);
             this.show_data.Multiline = true;
             this.show_data.Name = "show_data";
-            this.show_data.Size = new System.Drawing.Size(452, 150);
+            this.show_data.Size = new System.Drawing.Size(500, 50);
             this.show_data.TabIndex = 13;
+            this.show_data.Text = "empty, will be used to show results";
             // 
             // ro_female
             // 
             this.ro_female.AutoSize = true;
-            this.ro_female.Location = new System.Drawing.Point(163, 172);
+            this.ro_female.Location = new System.Drawing.Point(136, 172);
             this.ro_female.Name = "ro_female";
             this.ro_female.Size = new System.Drawing.Size(59, 17);
             this.ro_female.TabIndex = 12;
@@ -239,6 +263,120 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "User name ";
             // 
+            // lbl_selected_color
+            // 
+            this.lbl_selected_color.AutoSize = true;
+            this.lbl_selected_color.Location = new System.Drawing.Point(101, 211);
+            this.lbl_selected_color.Name = "lbl_selected_color";
+            this.lbl_selected_color.Size = new System.Drawing.Size(92, 13);
+            this.lbl_selected_color.TabIndex = 15;
+            this.lbl_selected_color.Text = "No Color Selected";
+            // 
+            // btn_pick_color
+            // 
+            this.btn_pick_color.Location = new System.Drawing.Point(11, 204);
+            this.btn_pick_color.Name = "btn_pick_color";
+            this.btn_pick_color.Size = new System.Drawing.Size(84, 23);
+            this.btn_pick_color.TabIndex = 16;
+            this.btn_pick_color.Text = "Choose Color";
+            this.btn_pick_color.UseVisualStyleBackColor = true;
+            this.btn_pick_color.Click += new System.EventHandler(this.btn_pick_color_Click);
+            // 
+            // dtp_birthdate
+            // 
+            this.dtp_birthdate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtp_birthdate.Location = new System.Drawing.Point(110, 232);
+            this.dtp_birthdate.Name = "dtp_birthdate";
+            this.dtp_birthdate.Size = new System.Drawing.Size(105, 20);
+            this.dtp_birthdate.TabIndex = 17;
+            // 
+            // lb_birth_day
+            // 
+            this.lb_birth_day.AutoSize = true;
+            this.lb_birth_day.Location = new System.Drawing.Point(18, 239);
+            this.lb_birth_day.Name = "lb_birth_day";
+            this.lb_birth_day.Size = new System.Drawing.Size(55, 13);
+            this.lb_birth_day.TabIndex = 18;
+            this.lb_birth_day.Text = "Birth Date";
+            // 
+            // cmb_country
+            // 
+            this.cmb_country.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_country.FormattingEnabled = true;
+            this.cmb_country.Items.AddRange(new object[] {
+            "Yemen",
+            "Egypt",
+            "Oman",
+            "Qatar",
+            "Palestine",
+            "Syria"});
+            this.cmb_country.Location = new System.Drawing.Point(109, 261);
+            this.cmb_country.Name = "cmb_country";
+            this.cmb_country.Size = new System.Drawing.Size(121, 21);
+            this.cmb_country.TabIndex = 19;
+            // 
+            // lb_country
+            // 
+            this.lb_country.AutoSize = true;
+            this.lb_country.Location = new System.Drawing.Point(18, 264);
+            this.lb_country.Name = "lb_country";
+            this.lb_country.Size = new System.Drawing.Size(85, 13);
+            this.lb_country.TabIndex = 20;
+            this.lb_country.Text = "Choose Country";
+            // 
+            // pic_student
+            // 
+            this.pic_student.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_student.Location = new System.Drawing.Point(374, 7);
+            this.pic_student.Name = "pic_student";
+            this.pic_student.Size = new System.Drawing.Size(120, 120);
+            this.pic_student.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_student.TabIndex = 21;
+            this.pic_student.TabStop = false;
+            // 
+            // btn__reset
+            // 
+            this.btn__reset.BackColor = System.Drawing.Color.Khaki;
+            this.btn__reset.Location = new System.Drawing.Point(104, 288);
+            this.btn__reset.Name = "btn__reset";
+            this.btn__reset.Size = new System.Drawing.Size(75, 24);
+            this.btn__reset.TabIndex = 22;
+            this.btn__reset.Text = "Reset";
+            this.btn__reset.UseVisualStyleBackColor = false;
+            this.btn__reset.Click += new System.EventHandler(this.btn__reset_Click);
+            // 
+            // btn_upload
+            // 
+            this.btn_upload.Location = new System.Drawing.Point(374, 128);
+            this.btn_upload.Name = "btn_upload";
+            this.btn_upload.Size = new System.Drawing.Size(120, 30);
+            this.btn_upload.TabIndex = 23;
+            this.btn_upload.Text = "Upload Picture";
+            this.btn_upload.UseVisualStyleBackColor = true;
+            this.btn_upload.Click += new System.EventHandler(this.btn_upload_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_save.Location = new System.Drawing.Point(13, 319);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(100, 30);
+            this.btn_save.TabIndex = 24;
+            this.btn_save.Text = "Save Data";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_load
+            // 
+            this.btn_load.BackColor = System.Drawing.Color.BurlyWood;
+            this.btn_load.Location = new System.Drawing.Point(119, 318);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(100, 30);
+            this.btn_load.TabIndex = 25;
+            this.btn_load.Text = "Load Data";
+            this.btn_load.UseVisualStyleBackColor = false;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -254,6 +392,7 @@
             this.Text = "Registrion_form";
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_student)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -280,6 +419,17 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_country;
+        private System.Windows.Forms.ComboBox cmb_country;
+        private System.Windows.Forms.Label lb_birth_day;
+        private System.Windows.Forms.DateTimePicker dtp_birthdate;
+        private System.Windows.Forms.Button btn_pick_color;
+        private System.Windows.Forms.Label lbl_selected_color;
+        private System.Windows.Forms.PictureBox pic_student;
+        private System.Windows.Forms.Button btn__reset;
+        private System.Windows.Forms.Button btn_upload;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_save;
     }
 }
 
--- Rigistrion_form/Form1.cs
+++ Rigistrion_form/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -23,6 +24,9 @@ namespace Rigistrion_form
         string id;
         string study_level;
         string gender;
+        string birth_day;
+        string color;
+        string country;
 
 
         private void btn_submit_Click(object sender, EventArgs e)
@@ -31,15 +35,19 @@ namespace Rigistrion_form
             password = lb_password.Text;
             email = lb_email.Text;
             id = lb_id.Text;
-            study_level = lb_email.Text;
+            study_level = lb_stu_level.Text;
             gender = ro_mule.Text;
+            birth_day = dtp_birthdate.Value.ToShortDateString();
+            color = lbl_selected_color.Text.Replace("Selected Color:","");
+            country = cmb_country.SelectedItem?.ToString()??"not selected ";
+
 
             if (!Regex.IsMatch(user_name, "\\w+\\s?\\w+(\\d)?"))
             {
                 MessageBox.Show("User name is unvalidion");
                 return;
             }
-            if (!Regex.IsMatch(password, "^\\d{5,}$"))
+            if (!Regex.IsMatch(password, "^\\d{6,}$"))
             {
                 MessageBox.Show("password is unvalidion");
                 return;
@@ -62,6 +70,7 @@ namespace Rigistrion_form
                 MessageBox.Show("study_level is unvalidion");
                 return;
             }
+
             if (gender == null)
             {
                 gender = "Female";
@@ -70,13 +79,108 @@ namespace Rigistrion_form
             {
                 gender = "Mule";
             }
-            
-          
-            show_data.Text = "User Name: "+user_name+"\n"
-                             +"Email: " +email+"\n"
-                             +"ID: "+id+"\n"
-                             +"Gender: "+gender;
+            if (cmb_country.SelectedItem == null)
+            {
+                MessageBox.Show("Pleas choose country!");
+                return;
+            }
+            if (lbl_selected_color.Text == "No Color Selected")
+            {
+                MessageBox.Show("Pleas choose favorite color!");
+            }
 
+            show_data.Text = $"User Name: {user_name}\nEmail: {email}\nPassword: {lb_password}\nID: {id}\nGender: {gender}\nstudy level: {lb_stu_level}" +
+                $"Birth day: {birth_day}\n Country: {country}\nFavorite Color: {color}";
+
+        }
+
+        private void btn_pick_color_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                lbl_selected_color.Text = $"Selected color: {colorDialog.Color.Name}";
+            }
+        }
+
+        private void btn__reset_Click(object sender, EventArgs e)
+        {
+            lb_username.Text = "";
+            lb_password.Text = "";
+            lb_email.Text = "";
+            lb_id.Text = "";
+            lb_stu_level.Text = "";
+            ro_mule.Checked = false;
+            ro_female.Checked = false;
+            cmb_country.SelectedIndex = -1;
+            dtp_birthdate.Value = DateTime.Now;
+            lbl_selected_color.Text = "No Color Selected";
+            show_data.Text = "";
+            pic_student.Image = null;
+            lb_username.Focus();
+        }
+
+        private void btn_upload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image.File|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pic_student.Image = Image.FromFile(openFileDialog.FileName);
+            }
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            string data = $"User Name: {user_name}\nEmail: {email}\nPassword: {lb_password}\nID: {id}\nGender: {gender}\nstudy level: {lb_stu_level}" +
+                $"Birth day: {birth_day}\n Country: {country}\nFavorite Color: {color}"; ;
+
+            File.WriteAllText("student_data.txt",data);
+            if(pic_student.Image != null)
+            {
+                pic_student.Image.Save("student_picture.jpg");
+            }
+            MessageBox.Show("Data saved succssfully! ");
+        }
+
+        private DateTimePicker GetDtp_birthdate()
+        {
+            return dtp_birthdate;
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("student_data.txt"))
+            {
+                MessageBox.Show("No saved data Found");
+                return;
+            }
+            string[] lines = File.ReadAllLines("student_data.txt");
+           /* if(lines.Length < 9)
+            {
+                MessageBox.Show("saved data in complate!");
+                return ;
+            }*/
+
+            lb_username.Text = lines[0];
+            lb_email.Text = lines[1];
+            lb_password.Text = lines[2];
+            lb_id.Text = lines[3];
+            if (lines[4]=="Mule") ro_mule.Checked = true;
+            else ro_female.Checked = true;
+           
+            lb_stu_level.Text = lines[5];
+            dtp_birthdate.Value = DateTime.Parse(lines[6]);
+            cmb_country.SelectedItem = lines[7];
+            lbl_selected_color.Text = "Selected Color: "+lines[8];
+            if (File.Exists("student_picture.jpg") && lines[9] == "student_picture.jpg")
+            {
+                pic_student.Image = Image.FromFile("student_picture.jpg");
+            }
+            else
+            {
+                pic_student.Image = null;
+            }
         }
     }
 }
